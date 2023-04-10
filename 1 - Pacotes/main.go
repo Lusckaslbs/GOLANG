4d28adb4129dc43409ed61dcf87dfc439144d5fb
Luckaslbs <p.lucasbatistasilvas@gmail.com>
2023-04-10 10:47:54 -0300

    commit

---
+++ 1 - Pacotes/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"modulo/auxiliar"
+
+	"github.com/badoux/checkmail"
+)
+
+func main() {
+	fmt.Println("Escrevendo do arquivo main")
+	auxiliar.Escrever()
+
+	erro := checkmail.ValidateFormat("lucas-lbds2011@hotmail.com")
+	fmt.Println(erro)
+}
