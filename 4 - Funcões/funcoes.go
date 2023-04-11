200c3abeb2b1ecb5b1ac4493d81f823defc7ec7b
Luckaslbs <p.lucasbatistasilvas@gmail.com>
2023-04-11 14:44:23 -0300

    commit

--- 4 - Funcões/funcoes.go
+++ 4 - Funcões/funcoes.go
@@ -6,6 +6,12 @@ func somar(n1 int8, n2 int8) int8 {
 	return n1 + n2
 }
 
+func calculosMatematicos(n1, n2 int8) (int8, int8) {
+	soma := n1 + n2
+	subtracao := n1 - n2
+	return soma, subtracao
+
+}
 func main() {
 	soma := somar(10, 20)
 	fmt.Println(soma)
@@ -17,4 +23,7 @@ func main() {
 
 	resultado := f("Texto da função 1")
 	fmt.Println(resultado)
+
+	resultadoSoma, _ := calculosMatematicos(10, 15)
+	fmt.Println(resultadoSoma)
 }
