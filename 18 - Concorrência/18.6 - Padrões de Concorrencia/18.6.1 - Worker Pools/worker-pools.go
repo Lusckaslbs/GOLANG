11ce09fea4276c6e90478114654552805f4bd740
Luckaslbs <p.lucasbatistasilvas@gmail.com>
2023-05-01 09:58:39 -0300

    commit

--- 18 - Concorrência/18.6 - Padrões de Concorrencia/18.6.1 - Worker Pools/worker-pools.go
+++ 18 - Concorrência/18.6 - Padrões de Concorrencia/18.6.1 - Worker Pools/worker-pools.go
@@ -1,7 +1,29 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	tarefas := make(chan int, 45)
+	resultados := make(chan int, 45)
+
+	go worker(tarefas, resultados)
+
+	for i := 0; i < 45; i++ {
+		tarefas <- i
+
+	}
+	close(tarefas)
+
+	for i := 0; i < 45; i++ {
+		resultado := <-resultados
+		fmt.Println(resultado)
+	}
+}
+
+func worker(tarefas <-chan int, resultados chan<- int) {
+	for numero := range tarefas {
+		resultados <- fibonacci(numero)
+	}
 }
 
 func fibonacci(posicao int) int {
